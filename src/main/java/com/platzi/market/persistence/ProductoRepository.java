ee2a28f956b3a9787f7a04b7c94e6e20a274fea4
jose molina <josmoli@gmail.com>
2020-10-26 20:53:35 -0500

    Se agrega Mapstruct

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,13 +2,35 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepositoryRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepositoryRepository.findAll();
     }
+    public List<Producto> getByCategoria(int idCategoria)
+    {
+        return productoCrudRepositoryRepository.findByIdCategoriaOrderByNombre(idCategoria);
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepositoryRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepositoryRepository.findById(idProducto);
+    }
+
+    public Producto save (Producto producto){
+        return productoCrudRepositoryRepository.save(producto);
+    }
+
+    public void delete (int idProducto){
+        productoCrudRepositoryRepository.deleteById(idProducto);
+    }
+
 }
